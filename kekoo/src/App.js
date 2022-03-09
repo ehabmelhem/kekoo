df8001ff3bfa126b890433d772db249ec25bbbfc
Manager data <rwed.kabaha@gmail.com>
2022-03-10 00:11:26 +0200

    Header final version

--- kekoo/src/App.js
+++ kekoo/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import "./View/Componenties/Header.css";
 
 // Imports The Compomenets:
-import Header from './View/Componenties/Header.js';
 function App() {
   return (
     <div className="App">
-      <Header></Header>
     </div>
   );
 }
--- kekoo/src/View/Componenties/Header.js
+++ kekoo/src/View/Componenties/Header.js
@@ -1,31 +1,37 @@
 import React from 'react'
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import "./Header.css"
 
 function Header() {
-  return (
-    <div> 
+    return (
         <header id="header" class="header">
-    <div class="container">
-      <div class="header__row">
-        <div class="header__logo">
-            {/* This is our logo */}
-          <a href="#">
-            <img src="" alt="" />
-          </a>
-          
-        </div>
-        <div class="header__content">
-          <nav class="header__nav">
-          </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+            <div class="container">
+                <div class="header__logo">
+                    {/* <a href="/">
+                        <img src="/Images\frontend24-white-171x30.png" alt="" />
+                    </a> */}
+                </div>
+                <form className="flex">
+                    <input
+                        autoFocus
+                        placeholder="todoapp..."
+                        type="text"
+                    />
+                    <IconButton
+                        type="submit"
+                    >
+                        <SearchIcon />
+                    </IconButton>
+                </form>
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
 
-  <main id="main">
-   
-  </main>
-    </div>
-  )
+            </div>
+        </header>
+    )
 }
 
 export default Header

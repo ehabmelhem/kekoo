222b3109b2c90caf70114e23f3ae4c88fe00972c
Ehab Melhem <melhemehab@gmail.com>
2022-03-09 22:44:09 +0200

    redux and react router

--- kekoo/src/App.js
+++ kekoo/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  console.log(useSelector((state) => state));
   return (
-    <div className="App">
-      
+    <div className="app">
     </div>
   );
 }
---
+++ kekoo/src/redux/reducers/todo.js
@@ -0,0 +1,10 @@
+const initialState = {
+    todos: [],
+};
+
+
+const todoReducer = (state = initialState, action) => {
+    return state;
+};
+
+export default todoReducer;

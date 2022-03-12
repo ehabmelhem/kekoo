576b4535ad2944e7eb6c58a00f72c651d07f71c4
Ehab Melhem <melhemehab@gmail.com>
2022-03-12 15:47:35 +0200

    add routing pages Home Category images Category Videos

--- kekoo/src/View/Componenties/Header.js
+++ kekoo/src/View/Componenties/Header.js
@@ -4,8 +4,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import MicIcon from '@mui/icons-material/Mic';
 import "./Header.css"
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const navigate = useNavigate();
+
     const [searchInput, setSearchInput] = React.useState('')
     const onClickSearchHandler = (e) => {
         e.preventDefault();
@@ -14,6 +20,27 @@ function Header() {
 
     const onChangeSearchInput = (e) => {
         setSearchInput(e.target.value);
+    }
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    const handleRedirectRouterImages = ()=> {
+        handleClose();
+        navigate('/gimages');
+    }
+    const handleRedirectRouterVideos = ()=> {
+        handleClose();
+        navigate('/gvideos');
+    }
+    const handleRedirectRouterHome = ()=> {
+        handleClose();
+        navigate('/');
     }
 
     return (
@@ -46,10 +73,29 @@ function Header() {
                         </IconButton>
                     </form>
                 </center>
-
-                <IconButton>
-                    <MoreVertIcon />
+                <IconButton
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
+                    <MoreVertIcon/>
                 </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <MenuItem onClick={handleRedirectRouterHome}>Home</MenuItem>
+                    <MenuItem onClick={handleRedirectRouterVideos}>Videos Category</MenuItem>
+                    <MenuItem onClick={handleRedirectRouterImages}>Images Category</MenuItem>
+                </Menu>
+
 
             </div>
         </header>

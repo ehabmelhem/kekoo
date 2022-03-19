9e6f5fb2a7bdf8f7c81555938efff78c5f97d162
Ehab Melhem <melhemehab@gmail.com>
2022-03-19 15:27:36 +0200

    little changes

--- kekoo/src/View/MainContent/Container.js
+++ kekoo/src/View/MainContent/Container.js
@@ -10,16 +10,16 @@ import "swiper/swiper.min.css";
 SwiperCore.use([EffectCoverflow, Pagination]);
 
 const videosUrl=[{
-    url:"https://v16-webapp.tiktok.com/1555e3f7cc28a3dd8a3bf37041287b8c/62303eb7/video/tos/alisg/tos-alisg-pve-0037c001/d0b1b640791b4c568f6153ab0f16e267/?a=1988&br=2026&bt=1013&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th1mdvDXq&l=202203150122170102450050271781A51F&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzo8bWc6Zm5uOzMzODczNEApZzg2Njg0ZjtmN2c1OTU3NGdjaC41cjRfL2VgLS1kMS1zc14uMF4zLTRiNjReNF40NWI6Yw%3D%3D&vl=&vr="
+    url:"https://v16-webapp.tiktok.com/3731ce8049cf0a51b34bbb7543e165a9/623620b9/video/tos/alisg/tos-alisg-pve-0037c001/f6781e308c194e579469efb5789b4d30/?a=1988&br=2644&bt=1322&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th1-Z_DXq&l=202203191227540101921680910ECC9414&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3k0ZGg6ZmRwOzMzODczNEApOWY2NzkzNGVmNzk1aTk3PGdjLS40cjQwaW1gLS1kMS1zc2EwNi40LjIzNjI2NC5jMmI6Yw%3D%3D&vl=&vr="
 },
 {
-    url:"https://v16-webapp.tiktok.com/58dd0f3a74e87945ab1947515470d24b/62303f01/video/tos/alisg/tos-alisg-pve-0037c001/557f6f9f69d2460ea2e206a6785dc449/?a=1988&br=1628&bt=814&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th_idvDXq&l=202203150123330102451300940D81EE86&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M29lajg6ZnRmOTMzODczNEApZTRpZzw5Z2VnNzlkZTU0Z2dzYy1gcjRfbHJgLS1kMS1zc2BiNWMyNWMyYl5gMzVhXjQ6Yw%3D%3D&vl=&vr="
+    url:"https://v16-webapp.tiktok.com/3731ce8049cf0a51b34bbb7543e165a9/623620b9/video/tos/alisg/tos-alisg-pve-0037c001/f6781e308c194e579469efb5789b4d30/?a=1988&br=2644&bt=1322&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th1-Z_DXq&l=202203191227540101921680910ECC9414&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3k0ZGg6ZmRwOzMzODczNEApOWY2NzkzNGVmNzk1aTk3PGdjLS40cjQwaW1gLS1kMS1zc2EwNi40LjIzNjI2NC5jMmI6Yw%3D%3D&vl=&vr="
 },
 {
-    url:"https://v16-webapp.tiktok.com/66019e12ceb58a97f40eabd32d784e17/62303f46/video/tos/useast2a/tos-useast2a-ve-0068c003/74fd517da3d444e4a44c0fc5e57bbde5/?a=1988&br=2032&bt=1016&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th_idvDXq&l=202203150123330102451300940D81EE86&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajxvZGllaGV1NjMzNzgzM0ApOjU8Z2hoaDw4N2U1ZTxmN2dqM2VpcC9xZ2ZgLS1kLzZzczEyMWNhLzUxYjViMy0uMjM6Yw%3D%3D&vl=&vr="
+    url:"https://v16-webapp.tiktok.com/3731ce8049cf0a51b34bbb7543e165a9/623620b9/video/tos/alisg/tos-alisg-pve-0037c001/f6781e308c194e579469efb5789b4d30/?a=1988&br=2644&bt=1322&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th1-Z_DXq&l=202203191227540101921680910ECC9414&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3k0ZGg6ZmRwOzMzODczNEApOWY2NzkzNGVmNzk1aTk3PGdjLS40cjQwaW1gLS1kMS1zc2EwNi40LjIzNjI2NC5jMmI6Yw%3D%3D&vl=&vr="
 },
 {
-    url:"https://v16-webapp.tiktok.com/66019e12ceb58a97f40eabd32d784e17/62303f46/video/tos/useast2a/tos-useast2a-ve-0068c003/74fd517da3d444e4a44c0fc5e57bbde5/?a=1988&br=2032&bt=1016&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th_idvDXq&l=202203150123330102451300940D81EE86&lr=tiktok&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajxvZGllaGV1NjMzNzgzM0ApOjU8Z2hoaDw4N2U1ZTxmN2dqM2VpcC9xZ2ZgLS1kLzZzczEyMWNhLzUxYjViMy0uMjM6Yw%3D%3D&vl=&vr="
+    url:"https://v16-webapp.tiktok.com/3731ce8049cf0a51b34bbb7543e165a9/623620b9/video/tos/alisg/tos-alisg-pve-0037c001/f6781e308c194e579469efb5789b4d30/?a=1988&br=2644&bt=1322&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-39Znz7Th1-Z_DXq&l=202203191227540101921680910ECC9414&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3k0ZGg6ZmRwOzMzODczNEApOWY2NzkzNGVmNzk1aTk3PGdjLS40cjQwaW1gLS1kMS1zc2EwNi40LjIzNjI2NC5jMmI6Yw%3D%3D&vl=&vr="
 },
 ]
 
--- kekoo/src/View/Router/Account/ProfilePage.js
+++ kekoo/src/View/Router/Account/ProfilePage.js
@@ -16,51 +16,109 @@ import { Link } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import AvatarPic from "./Componeneties/Avatar";
 import { flexbox } from "@material-ui/system";
-import Video from "../../MainContent/Video"
+import Video from "../../MainContent/Video";
 import "./ProfilePage.css";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import StandardImageList from "./StandardImageList";
 
-import StandardImageList from "./StandardImageList"
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   icon: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6)
+    padding: theme.spacing(8, 0, 6),
   },
   heroButtons: {
-    marginTop: theme.spacing(4)
+    marginTop: theme.spacing(4),
   },
   cardGrid: {
     paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
+    paddingBottom: theme.spacing(8),
   },
   card: {
     height: "100%",
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   cardContent: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6)
-  }
+    padding: theme.spacing(6),
+  },
 }));
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.substr(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
+const ProfileContent = () => {
+  return (
+    <div className="store__profile__content">
+      <Tooltip title="Ehab Melhem">
+        <IconButton size="small" aria-haspopup="true">
+          {/* Avatar for the company */}
+          <Avatar
+            style={{ width: "150px", height: "150px" }}
+            sx={{ width: 60, height: 60 }}
+            {...stringAvatar("Ehab Melhem")}
+          />
+        </IconButton>
+      </Tooltip>
+      <div className="store__profile__description">
+        <Typography variant="h6" gutterBottom component="div">
+          Store name
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom component="div">
+        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur
+      </Typography>
+      </div>
+    </div>
+  );
+};
 
 export default function ProfilePage() {
   const classes = useStyles();
 
   return (
     <div className="store__profile">
+      <ProfileContent />
+      <hr />
       <div className="store__profile__video__content">
-      <StandardImageList/>
+        <StandardImageList />
       </div>
     </div>
   );
